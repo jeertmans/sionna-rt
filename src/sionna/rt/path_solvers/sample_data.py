jeertmans/sionna-rt

commit 39e25ebab8d7eaf468046748c79dbdfc8015bb3f

Release v2.0.0
Signed-off-by: The Sionna Team <sionna@nvidia.com>
Merged-by: Guillermo Marcus <4169784+gmarcusm@users.noreply.github.com>

Co-authored-by: Jakob Hoydis <5190129+jhoydis@users.noreply.github.com>
Co-authored-by: Fayçal Ait-Aoudia <43564757+faycalaa@users.noreply.github.com>
Co-authored-by: Sebastian Cammerer <18167671+SebastianCa@users.noreply.github.com>
Co-authored-by: Lorenzo Maggi <34341780+lollodealma@users.noreply.github.com>
Co-authored-by: Merlin Nimier-David <merlin.nimier@gmail.com>
Co-authored-by: Baptiste Nicolet <40777524+bathal1@users.noreply.github.com>
Co-authored-by: Guillermo Marcus <4169784+gmarcusm@users.noreply.github.com>

src/sionna/rt/path_solvers/sample_data.py
@@ -1,5 +1,5 @@
 #
-# SPDX-FileCopyrightText: Copyright (c) 2021-2026 NVIDIA CORPORATION & AFFILIATES. All rights reserved.
+# SPDX-FileCopyrightText: Copyright (c) 2021-2025 NVIDIA CORPORATION & AFFILIATES. All rights reserved.
 # SPDX-License-Identifier: Apache-2.0
 #
 """Stores shooting and bouncing of rays samples data"""
@@ -112,12 +112,8 @@ class SampleData:
         shapes = dr.reinterpret_array(mi.UInt, shapes)
 
         # Store data in the buffer
-        data = SampleData.SampleDataFields(dr.detach(interaction_types),
-                                           dr.detach(shapes),
-                                           dr.detach(primitives),
-                                           dr.detach(vertices),
-                                           dr.detach(probs))
-
+        data = SampleData.SampleDataFields(interaction_types, shapes,
+                                           primitives, vertices, probs)
         self._local_mem.write(data, index, active=active)
 
         # Store the local edge index and edge properties

commit 5abba15a37b9f4598734766cc9236eb549a6d847

fix(lib): revert bug introduced in 39e25eb See: https://github.com/NVlabs/sionna-rt/issues/66

src/sionna/rt/path_solvers/sample_data.py
@@ -112,8 +112,13 @@ class SampleData:
         shapes = dr.reinterpret_array(mi.UInt, shapes)
 
         # Store data in the buffer
-        data = SampleData.SampleDataFields(interaction_types, shapes,
-                                           primitives, vertices, probs)
+        data = SampleData.SampleDataFields(
+            dr.detach(interaction_types),
+            dr.detach(shapes),
+            dr.detach(primitives),
+            dr.detach(vertices),
+            dr.detach(probs))
+
         self._local_mem.write(data, index, active=active)
 
         # Store the local edge index and edge properties